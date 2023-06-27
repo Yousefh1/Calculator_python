d2f28f26c8865459adf9539a85ecded53d560528
Yousefh1 <yhassan5252@gmail.com>
2023-06-26 21:58:44 -0400

    intial commit

---
+++ cal-py.py
@@ -0,0 +1,48 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
+print("Welcome to the Calculator Program!")
+
+while True:
+    print("\nSelect an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '5':
+        print("Goodbye!")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result: ", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result: ", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result: ", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result: ", result)
+    else:
+        print("Invalid choice. Please try again.")
